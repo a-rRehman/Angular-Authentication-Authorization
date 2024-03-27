a4e95764323e84b249f4cfc08da789e936b8a971
a-rRehman <70515831+a-rRehman@users.noreply.github.com>
2024-03-27 12:16:15 +0500

    Template Driven Form made

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent {
-
+  isLoginMode: boolean = true;
+  onswitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+  onFormSubmitted(form: NgForm) {
+    console.log(form.value);
+    form.reset();
+  }
 }
