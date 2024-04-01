d8a5c09de624cdf5a48325ff4d654afe846a0625
a-rRehman <70515831+a-rRehman@users.noreply.github.com>
2024-04-01 16:31:30 +0500

    Added signup and signin api with handeling error simply

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable, inject } from "@angular/core";
 import { AuthResponse } from "../Model/AuthResponse";
+import { catchError, throwError } from "rxjs";
 
 @Injectable({
   providedIn: "root",
@@ -14,9 +15,57 @@ export class AuthService {
       password: password,
       returnSecureToken: true,
     };
-    return this.http.post<AuthResponse>(
-      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCUTQ8b0XNZ1L5sXQuGL06FA2JZiAaWPS8",
-      data
-    );
+    return this.http
+      .post<AuthResponse>(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCUTQ8b0XNZ1L5sXQuGL06FA2JZiAaWPS8",
+        data
+      )
+      .pipe(
+        catchError((err) => {
+          let errorMessage = "An unknown error has occured";
+          if (!err.error || !err.error.error) {
+            return throwError(() => errorMessage);
+          }
+          switch (err.error.error.message) {
+            case "EMAIL_EXISTS":
+              errorMessage = "This email already exists";
+              break;
+            case "INVALID_LOGIN_CREDENTIALS":
+              errorMessage = "INVALID_LOGIN_CREDENTIALS";
+              break;
+          }
+          return throwError(() => errorMessage);
+        })
+      );
+  }
+
+  login(email, password) {
+    const data = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    return this.http
+      .post<AuthResponse>(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCUTQ8b0XNZ1L5sXQuGL06FA2JZiAaWPS8",
+        data
+      )
+      .pipe(
+        catchError((err) => {
+          let errorMessage = "An unknown error has occured";
+          if (!err.error || !err.error.error) {
+            return throwError(() => errorMessage);
+          }
+          switch (err.error.error.message) {
+            case "EMAIL_EXISTS":
+              errorMessage = "This email already exists";
+              break;
+            case "INVALID_LOGIN_CREDENTIALS":
+              errorMessage = "INVALID_LOGIN_CREDENTIALS";
+              break;
+          }
+          return throwError(() => errorMessage);
+        })
+      );
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,6 +12,7 @@ export class LoginComponent {
   authService: AuthService = inject(AuthService);
   isLoginMode: boolean = true;
   isLoading: Boolean = false;
+  errorMessage: string | null = null;
   onswitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
@@ -19,7 +20,19 @@ export class LoginComponent {
     const email = form.value.email;
     const password = form.value.password;
     if (this.isLoginMode) {
-      return;
+      this.isLoading = true;
+      this.authService.login(email, password).subscribe({
+        next: (response) => {
+          console.log("The Response is :", response);
+          this.isLoading = false;
+        },
+        error: (errMsg) => {
+          console.log("The Error is ", errMsg);
+          this.isLoading = false;
+          this.errorMessage = errMsg;
+          this.hideSnackbar();
+        },
+      });
     } else {
       this.isLoading = true;
       this.authService.signup(email, password).subscribe({
@@ -27,13 +40,20 @@ export class LoginComponent {
           console.log("The Response is :", response);
           this.isLoading = false;
         },
-        error: (error) => {
-          console.log("The Error is ", error);
+        error: (errMsg) => {
+          console.log("The Error is ", errMsg);
           this.isLoading = false;
+          this.errorMessage = errMsg;
+          this.hideSnackbar();
         },
       });
     }
     console.log(form.value);
     form.reset();
+  }
+  hideSnackbar() {
+    setTimeout(() => {
+      this.errorMessage = null;
+    }, 3000);
   }
 }
