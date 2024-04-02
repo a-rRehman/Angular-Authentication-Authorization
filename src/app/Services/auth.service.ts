13d957932a2c515d3459ca9c1764438ac5f5e15a
a-rRehman <70515831+a-rRehman@users.noreply.github.com>
2024-04-02 15:27:46 +0500

    Creating User Response Data

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,13 +1,15 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable, inject } from "@angular/core";
 import { AuthResponse } from "../Model/AuthResponse";
-import { catchError, throwError } from "rxjs";
+import { Subject, catchError, tap, throwError } from "rxjs";
+import { User } from "../Model/User";
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
   http: HttpClient = inject(HttpClient);
+  user = new Subject<User>();
 
   signup(email, password) {
     const data = {
@@ -35,8 +37,18 @@ export class AuthService {
               break;
           }
           return throwError(() => errorMessage);
+        }),
+        tap((res) => {
+          this.handleCreateUser(res);
         })
       );
+  }
+
+  private handleCreateUser(res) {
+    const expiresInTs = new Date().getTime() + +res.expiresIn * 1000;
+    const expiresIn = new Date(expiresInTs);
+    const user = new User(res.email, res.localId, res.idToken, expiresIn);
+    this.user.next(user);
   }
 
   login(email, password) {
@@ -51,21 +63,26 @@ export class AuthService {
         data
       )
       .pipe(
-        catchError((err) => {
-          let errorMessage = "An unknown error has occured";
-          if (!err.error || !err.error.error) {
-            return throwError(() => errorMessage);
-          }
-          switch (err.error.error.message) {
-            case "EMAIL_EXISTS":
-              errorMessage = "This email already exists";
-              break;
-            case "INVALID_LOGIN_CREDENTIALS":
-              errorMessage = "INVALID_LOGIN_CREDENTIALS";
-              break;
-          }
-          return throwError(() => errorMessage);
+        catchError(this.handleError),
+        tap((res) => {
+          this.handleCreateUser(res);
         })
       );
+  }
+
+  private handleError(err) {
+    let errorMessage = "An unknown error has occured";
+    if (!err.error || !err.error.error) {
+      return throwError(() => errorMessage);
+    }
+    switch (err.error.error.message) {
+      case "EMAIL_NOT_FOUND":
+        errorMessage = "This email doesnot exists";
+        break;
+      case "INVALID_LOGIN_CREDENTIALS":
+        errorMessage = "INVALID_LOGIN_CREDENTIALS";
+        break;
+    }
+    return throwError(() => errorMessage);
   }
 }
